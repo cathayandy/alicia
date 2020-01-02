2956c9276569c28c4caa1a0fd4264c1f8145fd16
cathayandy <wzm_andy@126.com>
2020-01-03 02:02:36 +0800

    fix bug

--- server/models/captcha.js
+++ server/models/captcha.js
@@ -1,14 +1,14 @@
 module.exports = (sequelize, Sequelize) => {
     const Captcha = sequelize.define('Captcha', {
         email: {
-            type: Sequelize.STRING,
+            type: Sequelize.STRING(100),
             validate: {
                 isEmail: true,
             },
             primaryKey: true,
         },
         captcha: {
-            type: Sequelize.STRING,
+            type: Sequelize.STRING(100),
         },
     });
     return Captcha;
