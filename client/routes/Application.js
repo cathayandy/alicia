4ac323cedb58f833934eeb8949734b8c0a4c135a
cathayandy <wzm_andy@126.com>
2020-03-01 15:36:55 +0800

    fix upload style

--- client/routes/Application.js
+++ client/routes/Application.js
@@ -245,9 +245,12 @@ class Application extends PureComponent {
                                     <Icon type="inbox" />
                                 </p>
                                 <p className="ant-upload-text">
-                                    请上传您的免修凭证（仅支持图片及PDF格式）
+                                    请上传您的免修凭证
                                 </p>
                                 <p className="ant-upload-hint">
+                                    仅支持图片及PDF格式
+                                </p>
+                                <p className="ant-upload-hint warn">
                                     申请人须保证上传材料的真实性
                                 </p>
                             </Dragger>
